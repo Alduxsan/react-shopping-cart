6c583d844469bac944b64f2c46cb43db547d31fa
Alduxsan <aldosantarelli@gmail.com>
2021-10-03 23:28:02 -0300

    corrections

--- src/dummyDB/vegetablesList.js
+++ src/dummyDB/vegetablesList.js
@@ -3,8 +3,8 @@ const vegetableList = [
   { name: 'Arvejas', price: 2600, img: '/productos/arvejas.jpg' },
   { name: 'Lechuga', price: 600, img: '/productos/lechuga.jpg' },
   { name: 'Banana', price: 1500, img: '/productos/banana.jpg' },
-  { name: 'naranja', price: 2600, img: '/productos/naranja.jpeg' },
-  { name: 'frutilla', price: 600, img: '/productos/frutilla.jpeg' },
+  { name: 'Naranja', price: 2600, img: '/productos/naranja.jpeg' },
+  { name: 'Frutilla', price: 600, img: '/productos/frutilla.jpeg' },
   { name: 'Tomate cherry', price: 1500, img: '/productos/cherry.jpeg' },
   { name: 'Habas', price: 2600, img: '/productos/habas.jpg' },
   {
